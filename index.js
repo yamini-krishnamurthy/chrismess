55205ff84300567bc47f7cee4c389249e947e544
Yamini Krishnamurthy <ykrishna@purdue.edu>
2018-06-25 13:36:34 -0400

    Clicking the button changes the heading

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('It works!');
+const button = document.querySelector('button')
+
+const changeHeading = function() {
+  const heading = document.querySelector('#heading 2');
+  heading.textContent = 'Heading changed!';
+}
+
+button.addEventListener('click', changeHeading)
