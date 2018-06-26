0860a0bd91096ac3b245957972bb4692606427f6
Yamini Krishnamurthy <ykrishna@purdue.edu>
2018-06-26 14:22:01 -0400

    Display the title in bold and release year in italics. Break down event handler into multiple functions

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const form = document.querySelector('#movieform')
 
+const renderTitle = function(event) {
+  const title = document.createElement('b')
+  title.textContent = event.target.moviename.value + ' '
+  return title
+}
+
+const renderYear = function(event) {
+  const year = document.createElement('em')
+  year.textContent = event.target.releaseyear.value
+  return year
+}
+
+const renderListElement = function(event) {
+  console.log(event.target.moviename.value)
+  //using createElement is more safe as compared to innerHTML
+  const listElement = document.createElement('li')
+  listElement.appendChild(renderTitle(event))
+  listElement.appendChild(renderYear(event))
+  return listElement
+}
+
 const addMovie = function(event) {
   //prevents the form from actually submitting
   event.preventDefault()
   console.log(event.target.moviename.value)
-  const listItem = document.createElement('li')
-  listItem.textContent = event.target.moviename.value + ' ' + event.target.releaseyear.value
-  document.querySelector('#movies').appendChild(listItem)
+  document.querySelector('#movies').appendChild(renderListElement(event))
   //clears text box
   form.reset()
 }
